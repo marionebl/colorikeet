076361e0800ac43986b9f7404d7d5b3a5ae50fc2
Mario Nebl <mnebl@atlassian.com>
2020-05-08 09:16:29 +1000

    test: more tests

--- src/colorikeet-from.test.ts
+++ src/colorikeet-from.test.ts
@@ -4,8 +4,14 @@ jest.unmock("./colorikeet");
 jest.unmock("./hsla");
 jest.unmock("./rgba");
 
-test("color.hex returns expected result", () => {
-  const color = Colorikeet.from('#0433FF');
+test("Colorikeet.from returns expected result", () => {
+  const color = Colorikeet.from("#0433FF");
   Colorikeet.assert(color);
   expect(color.hex).toEqual({ r: "04", g: "33", b: "ff" });
+});
+
+test("Colorikeet.from returns expected result", () => {
+  const color = Colorikeet.from("#00fdff");
+  Colorikeet.assert(color);
+  expect(color.hex).toEqual({ r: "00", g: "fd", b: "ff" });
 });
